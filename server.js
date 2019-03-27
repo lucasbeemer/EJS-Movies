f36fcbcffbc78815066c352cb6da213b2c6032f0
Lucas Beemer <lucasbeemer@gmail.com>
2019-03-27 00:03:02 -0700

    New movie added to database SUCCESS

--- server.js
+++ server.js
@@ -10,14 +10,27 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
 
+// GET all data
 app.get('/', (req, res) => {
     knex('movies').then((results) => {
         res.render('index', {movies: results});
     })
 })
 
+// GET add new data page
 app.get('/movies/new', (req, res) => {
     res.render('new_movie');
+})
+
+// CREATE a piece of data
+app.post('/movies', (req, res) => {
+    knex('movies').insert({
+        title: req.body.title,
+        description: req.body.description,
+        genre: req.body.genre
+    }).then(() => {
+        res.redirect('/')
+    })
 })
 
 app.listen(port, () => {
